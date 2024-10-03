3e9549dd5dc335515cdd8ab6a910b1dee0812b2b
Hannah Yoo <hyoo@mica.edu>
2024-10-03 13:46:20 -0400

    Build's complete

---
+++ Assets/Scripts/Example.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Example
+{
+    void Start()
+    {
+        //  For PC, Mac, Mobile: Let the device determine the framerate
+        //  WebGL Builds: let the browser set the frame rate 
+        Application.targetFrameRate = -1;   // -1 is a magic number
+    }
+}
+
+
